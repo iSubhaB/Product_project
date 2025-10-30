2f11ad3841c55e910d9764c21957c0527ed71664
suvo biswas <suvobiswas.bwn@gmail.com>
2025-10-30 10:16:23 +0530

    first commit

---
+++ BackEnd/src/server.js
@@ -0,0 +1,24 @@
+// server.js - main entry
+const express = require('express');
+const cors = require('cors');
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+
+
+
+
+// root
+app.get('/', (req, res) => {
+  res.send('Product Management Backend is running.');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
