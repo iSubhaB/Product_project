bcfa826f702b0eed91cfb6e4a23093f4d7d0ba15
suvo biswas <suvobiswas.bwn@gmail.com>
2025-10-30 12:17:50 +0530

    React app Create

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -5,12 +5,12 @@ import path from "path";
 import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
 
-// Routes
+
 import productRoutes from "./routes/productRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import subCategoryRoutes from "./routes/subCategoryRoutes.js";
 
-// Middlewares
+
 import errorHandler from "./middlewares/errorHandler.js";
 
 dotenv.config();
@@ -18,27 +18,26 @@ connectDB();
 
 const app = express();
 
-// Required for ES module __dirname resolution
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static images
+
 app.use("/uploads", express.static(path.join(__dirname, "../../uploads")));
 
-// routes
+
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/subcategories", subCategoryRoutes);
 
-// Test route
+
 app.get("/", (req, res) => res.send("API running ✅"));
 
-// Error handler (last)
+
 app.use(errorHandler);
 
 export default app;
