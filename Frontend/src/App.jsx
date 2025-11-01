b7ff881a11813e247bd99b62bc134d4f4dc0ce3e
suvo biswas <suvobiswas.bwn@gmail.com>
2025-11-01 12:38:47 +0530

    Delete and edit button upgrade

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,10 @@
-import { Rules } from "./components/Rules/Rules";
-
+import ProductList from "./pages/ProductList";
 
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="max-w-6xl mx-auto p-5">     
   <ProductList /> 
-=======
-    <div className="max-w-6xl mx-auto p-5">
-      <h1 className="text-2xl font-bold mb-5 text-center">Product Management</h1>
-     
-      <Rules/>
-
->>>>>>> 027655adcfeb1b5978d2393bfd80262b556d7cb3
     </div>
   );
 }
