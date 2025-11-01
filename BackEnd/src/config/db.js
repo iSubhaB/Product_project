2ed8860d98f51d65eb369bfb3ac5343340b578c9
suvo biswas <suvobiswas.bwn@gmail.com>
2025-11-01 17:51:31 +0530

    .env mongo link

--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://isubhab:isubhab@cluster0.inkxhay.mongodb.net/?appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    console.log(` MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    console.error(` MongoDB Connection Error: ${error.message}`);
     process.exit(1);
   }
 };
@@ -20,20 +20,3 @@ const connectDB = async () => {
 export default connectDB;
 
 
-
-
-
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//     try {
-//         // const conn = await mongoose.connect(process.env.MONGO_URI);
-//         const conn = await mongoose.connect("mongodb://localhost:27017/products");
-//         console.log(`MongoDB Connected: ${conn.connection.host}`);
-//     } catch (error) {
-//         console.error(`Error: ${error.message}`);
-//         process.exit(1);
-//     }
-// };
-
-// export default connectDB;
